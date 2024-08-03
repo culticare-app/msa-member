67ece5888b2d4d4a0273a5dd4f5d05886c534a57
Leeyoujin00 <cymn6032@gmail.com>
2024-08-03 15:52:28 +0900

    [CHORE] 인증 sms 발신자 수정#15

--- src/main/java/com/culticare/sms/util/SmsCertificationUtil.java
+++ src/main/java/com/culticare/sms/util/SmsCertificationUtil.java
@@ -34,7 +34,7 @@ public class SmsCertificationUtil {
         // 발신번호 및 수신번호는 반드시 01012345678 형태로 입력되어야 합니다.
         message.setFrom(senderNumber);
         message.setTo(to);
-        message.setText("[Kids In Seoul] 아래의 인증번호를 입력해주세요\n" + verificationCode);
+        message.setText("[CultiCare] 아래의 인증번호를 입력해주세요\n" + verificationCode);
 
         SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
         return response;
